05c90a8cbac573e738c81b5d5877730de9902ede
PeterSonne <papa@deep2chill.de>
2020-03-04 15:53:18 +0800

    Added controllers, filtered Songs request added

--- controllers/artists.js
+++ controllers/artists.js
@@ -1,8 +1,27 @@
-const router = require('express').Router()
-const db = require('../db')
+const router = require("express").Router();
+const db = require("../db");
 
-router.get('/', (req, res) => {})
+router.get("/", (req, res) => {
+  db.query("SELECT * FROM artists", (err, results) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(results);
+    }
+  });
+});
 
-router.get('/:id', (req, res) => {})
+router.get("/:id", (req, res) => {
+  db.query(
+    `SELECT * FROM artists WHERE id=${req.params.id}`,
+    (err, results) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send(results.length == 1 ? results[0] : {});
+      }
+    }
+  );
+});
 
-module.exports = router
+module.exports = router;
--- controllers/songs.js
+++ controllers/songs.js
@@ -1,6 +1,26 @@
-const router = require('express').Router()
-const db = require('../db')
+const router = require("express").Router();
+const db = require("../db");
 
-router.get('/', (req, res) => {})
+router.get("/", (req, res) => {
+  db.query(
+    `SELECT s.id as id, s.name as name, s.audio as audio, al.name as album, ar.name as artist, 
+    g.name as genre FROM songs as s LEFT JOIN albums as al ON s.album = al.id LEFT JOIN artists 
+    as ar ON s.artist = ar.id LEFT JOIN genres as g ON s.genre = g.id ${
+      Object.entries(req.query).length > 0
+        ? "WHERE " +
+          Object.keys(req.query)
+            .map(e => `s.${e} = ${req.query[e]}`)
+            .join(" AND ")
+        : ""
+    }`,
+    (err, results) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send(results);
+      }
+    }
+  );
+});
 
-module.exports = router
+module.exports = router;
