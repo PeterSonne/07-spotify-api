809393abed362c7ac77667a3fd72bd060f2a2aa6
PeterSonne <papa@deep2chill.de>
2020-03-05 16:38:14 +0800

    Payment controller added

--- controllers/albums.js
+++ controllers/albums.js
@@ -1,8 +1,22 @@
-const router = require('express').Router()
-const db = require('../db')
+const router = require("express").Router();
+const db = require("../db");
 
-router.get('/', (req, res) => {})
+router.get("/", (req, res) => {
+  db.query(
+    `SELECT al.id as id, al.name as name, ar.name as artist, al.cover as cover FROM albums as al 
+    LEFT JOIN artists ar ON al.artist = ar.id`,
+    (err, results) => {
+      res.status(err ? 500 : 200).send(err ? err : results);
+    }
+  );
+});
 
-router.get('/:id', (req, res) => {})
+router.get("/:id", (req, res) => {
+  db.query(
+    `SELECT al.id as id, al.name as name, ar.name as artist, al.cover as cover FROM albums as al 
+    LEFT JOIN artists ar ON al.artist = ar.id WHERE al.id=${req.params.id}`,
+    (err, results) => res.status(err ? 500 : 200).send(err ? err : results[0])
+  );
+});
 
-module.exports = router
+module.exports = router;
--- controllers/pay.js
+++ controllers/pay.js
@@ -1,5 +1,22 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const stripe = require("stripe")(process.env.STRIPE_SK);
 
-router.post('/', (req, res) => {})
+router.post("/", (req, res) => {
+  stripe.charges
+    .create({
+      amount: 9.99,
+      currency: "usd",
+      description: "Tortuga Spotify Exercise",
+      source: req.body
+    })
+    .then(data => {
+      console.log(data);
+      res.status(406).send({ error: { content: "Error" } });
+    })
+    .catch(err => {
+      console.log(err);
+      res.send(err);
+    });
+});
 
-module.exports = router
+module.exports = router;
