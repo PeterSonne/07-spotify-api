598816ba2892cd0e03c829f6d2bed85ce5a579f5
PeterSonne <papa@deep2chill.de>
2020-03-09 22:17:07 +0800

    Added Cover to Songs controller

--- controllers/songs.js
+++ controllers/songs.js
@@ -3,7 +3,7 @@ const db = require("../db");
 
 router.get("/", (req, res) => {
   db.query(
-    `SELECT s.id as id, s.name as name, s.audio as audio, al.name as album, ar.name as artist, 
+    `SELECT s.id as id, s.name as name, s.audio as audio, al.name as album, al.cover as cover, ar.name as artist, 
     g.name as genre FROM songs as s LEFT JOIN albums as al ON s.album = al.id LEFT JOIN artists 
     as ar ON s.artist = ar.id LEFT JOIN genres as g ON s.genre = g.id ${
       Object.entries(req.query).length > 0
